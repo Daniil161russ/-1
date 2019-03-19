db115861bcf7ae7e9a08074de53bc4c4e1ec9b92
Daniil <43216069+Daniil161russ@users.noreply.github.com>
2019-03-19 13:19:12 +0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$n1 = 1;
+$n2 = 2;
+$n3 = 3;
+$n4 = 4;
+$n5 = 5;
+
+
+echo $n1;
+echo '<br>';
+echo $n2;
+echo '<br>';
+echo $n3;
+echo '<br>';
+echo $n4;
+echo '<br>';
+echo $n5;
+echo '<br>';
+
+function replace(){
+	$n1 = 'one';
+	echo '<br>';
+	echo $n1;
+	$n2 = 'two';
+	echo '<br>';
+	echo $n2;
+	$n3 = 'three';
+	echo '<br>';
+	echo $n3;
+	$n4 = 'four';
+	echo '<br>';
+	echo $n4;
+	$n5 = 'five';
+	echo '<br>';
+	echo $n5;
+};
+
+replace();
+
+?>
